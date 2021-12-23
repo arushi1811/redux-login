a0fb2a79134a9d7fc48bdc265336c0b1a30f20a4
arushi1811 <arushig@bu.edu>
2021-12-22 19:00:05 -0600

    added redux dev tools

--- src/state/store.js
+++ src/state/store.js
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import reducers from "./reducers"
+import { composeWithDevTools } from "redux-devtools-extension";
+
 
 export const store = createStore(
     reducers, 
-    {},
-    applyMiddleware(thunk)
+    composeWithDevTools(applyMiddleware(thunk)),
+    // {},
+    // applyMiddleware(thunk)
 )
 
